4772bb149d0da9bf3cb7269f5a694e4f059fc8b4
Andrea Troianiello <andrea.troaniello.94@gmail.com>
2019-05-23 21:18:54 +0200

    Fix formfields bug

--- lib/view/book/authors-section.dart
+++ lib/view/book/authors-section.dart
@@ -10,6 +10,17 @@ class AuthorsSectionWidget extends StatefulWidget {
 }
 
 class _AuthorsSectionWidgetState extends State<AuthorsSectionWidget> {
+  Map<Author, Widget> mapForms;
+
+  _AuthorsSectionWidgetState() {
+    mapForms = Map<Author, Widget>();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _addForms();
+  }
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -26,52 +37,51 @@ class _AuthorsSectionWidgetState extends State<AuthorsSectionWidget> {
                   newAuthor.name = 'Nome';
                   newAuthor.surname = 'Cognome';
                   widget.authors.add(newAuthor);
+                  _addForms();
                 }),
           ),
         ),
-        ..._buildAuthorsForms(),
+        ...mapForms.values,
       ],
     );
   }
 
-  List<Widget> _buildAuthorsForms() {
-    List<Widget> forms = new List<Widget>();
-    for (Author author in widget.authors) {
-      forms.add(
-        ListTile(
-          leading: Icon(Icons.person),
-          title: TextFormField(
-            decoration: InputDecoration(
-              hintText: author.toString(),
+  void _addForms() {
+    for(Author author in widget.authors)
+    mapForms.putIfAbsent(
+      author,
+      () => ListTile(
+            key: Key(DateTime.now().toString()),
+            leading: Icon(Icons.person),
+            title: TextFormField(
+              initialValue: author.toString(),
+              validator: (String text) {
+                RegExp regExp =
+                    RegExp(r"^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$");
+                if (!text.contains(' ') || !regExp.hasMatch(text))
+                  return 'Author must have a name and a surname';
+                else
+                  return null;
+              },
+              onSaved: (String text) {
+                List<String> strings = text.split(' ');
+                author.clear();
+                author.name = strings.removeAt(0);
+                author.surname = strings.removeLast();
+                author.secondName = "";
+                for (String string in strings)
+                  author.secondName += " $string";
+              },
             ),
-            initialValue: author.toString(),
-            validator: (String text) {
-              RegExp regExp =
-                  RegExp(r"^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$");
-              if (!text.contains(' ') || !regExp.hasMatch(text))
-                return 'Author must have a name and a surname';
-              else
-                return null;
-            },
-            onSaved: (String text) {
-              List<String> strings = text.split(' ');
-              author.clear();
-              author.name = strings.removeAt(0);
-              author.surname = strings.removeLast();
-              author.secondName = "";
-              for (String string in strings) author.secondName += " $string";
-            },
+            trailing: IconButton(
+              icon: Icon(Icons.delete),
+              onPressed: () => setState(() {
+                    widget.authors.remove(author);
+                    mapForms.remove(author);
+                    print(widget.authors.toString());
+                  }),
+            ),
           ),
-          trailing: IconButton(
-            icon: Icon(Icons.delete),
-            onPressed: () => setState(() {
-                  widget.authors.remove(author);
-                  print(widget.authors.toString());
-                }),
-          ),
-        ),
-      );
-    }
-    return forms;
+    );
   }
 }
