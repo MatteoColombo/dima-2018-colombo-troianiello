dcb50a7dc0339cf830100e25fec5f1ac02e23168
MatteoColombo <matte.colo@gmail.com>
2019-08-14 16:36:28 +0200

    make buttons text uppercase

--- lib/view/common/confirm-dialog.dart
+++ lib/view/common/confirm-dialog.dart
@@ -12,12 +12,12 @@ class ConfirmDialog extends StatelessWidget {
         ButtonBar(
           children: <Widget>[
             FlatButton(
-              child: Text(Localization.of(context).cancel),
+              child: Text((Localization.of(context).cancel).toUpperCase()),
               onPressed: () => Navigator.of(context).pop(false),
             ),
             FlatButton(
               textColor: Theme.of(context).primaryColor,
-              child: Text(Localization.of(context).confirm),
+              child: Text((Localization.of(context).confirm).toUpperCase()),
               onPressed: () => Navigator.of(context).pop(true),
             ),
           ],
